abc48926bda22b6b0705a17836a48b5fb8390c77
akhanal1 <sraashis@gmail.com>
2019-02-22 16:37:45 -0500

    Added

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
-setup(name='image_utils',
+setup(name='imgcommons',
       version='0.1',
       description='Simple but very useful image related utilities',
-      url='https://github.com/sraashis/img_utils',
+      url='https://github.com/sraashis/image_utils',
+      download_url='https://github.com/sraashis/image_utils/releases/tag/0.1',
       author='Aashis Khanal',
       author_email='sraahis@gmail.com',
       license='MIT',
@@ -11,7 +12,7 @@ setup(name='image_utils',
       install_requires=['numpy', 'PILLOW', 'scipy', 'opencv-python'],
       classifiers=[
           "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: MIT License",
+          'License :: OSI Approved :: MIT License',
           "Operating System :: OS Independent",
       ],
       zip_safe=False)
